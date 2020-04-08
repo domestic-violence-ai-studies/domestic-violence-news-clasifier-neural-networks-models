98a907a50e6d2a4e6ed0415316dac9a803d21aef
hugo <hjbello.wk@gmail.com>
2020-04-08 13:26:11 +0200

    initial commit

---
+++ compress_bundle_json_file.sh
@@ -0,0 +1,4 @@
+cd data/json_bundle_news_domestic_violence
+#gunzip -ck large-bundle-corona.json > reviews-180k-bundle-after-corona.json.gz
+zip no_rep_news-domestic-no-violence.zip no_rep_news-domestic-no-violence.json 
+zip no_rep_news-domestic-violence.zip no_rep_news-domestic-violence.json 
